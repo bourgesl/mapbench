78effb784bfbabca3d78ae0c5597abc82b6ed308
bourgesl <bourges.laurent@gmail.com>
2014-01-27 07:23:17 +0100

    Added Linux 64 (Ubuntu 13.10) benchmark results

--- bin/bench_marlin.sh
+++ bin/bench_marlin.sh
@@ -2,15 +2,18 @@
 # get paths
 source ./env.sh
 
+# server jvm 
 JAVA_OPTS="-server -XX:+PrintCommandLineFlags -XX:+PrintFlagsFinal"
+# headless mode
+#JAVA_OPTS="-server -Djava.awt.headless=true -XX:+PrintCommandLineFlags -XX:+PrintFlagsFinal"
 #JAVA_OPTS="-XX:+PrintCommandLineFlags -XX:+PrintFlagsFinal"
 
 #JAVA_TUNING=""
 #JAVA_TUNING=" -Xms128m  -Xmx128m -XX:-TieredCompilation"
 #JAVA_TUNING=" -Xms128m  -Xmx128m -XX:+TieredCompilation"
 #JAVA_TUNING=" -Xms128m  -Xmx128m -XX:+AggressiveOpts -XX:CompileThreshold=1000"
-JAVA_TUNING=" -Xms2048m  -Xmx2048m"
-#JAVA_TUNING=" -Xms2048m  -Xmx2048m -ea -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC"
+#JAVA_TUNING=" -Xms2048m  -Xmx2048m"
+JAVA_TUNING=" -Xms2048m  -Xmx2048m -ea -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC"
 
 # MapBench:
 CLASSPATH=$MAP_BENCH_JAR

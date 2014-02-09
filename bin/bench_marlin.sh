0abec0791293df54cb0d75c71415cc3834aa933a
bourgesl <bourges.laurent@gmail.com>
2014-02-09 21:58:38 +0100

    Improved shell scripts, added scaleTest benchmark results on i5 machine

--- bin/bench_marlin.sh
+++ bin/bench_marlin.sh
@@ -11,22 +11,24 @@ JAVA_OPTS="-server -XX:+PrintCommandLineFlags -XX:+PrintFlagsFinal"
 #JAVA_TUNING=" -Xms256m  -Xmx256m -XX:-TieredCompilation"
 #JAVA_TUNING=" -Xms128m  -Xmx128m -XX:+TieredCompilation"
 #JAVA_TUNING=" -Xms128m  -Xmx128m -XX:+AggressiveOpts -XX:CompileThreshold=1000"
+
 JAVA_TUNING=" -Xms2048m  -Xmx2048m -XX:+UseConcMarkSweepGC"
 #JAVA_TUNING=" -Xms2048m  -Xmx2048m -ea -XX:+AggressiveOpts -XX:+UseConcMarkSweepGC"
 
 # MapBench:
 CLASSPATH=$MAP_BENCH_JAR
 
-# Marlin 0.3 in lib folder:
+# Marlin renderer in lib folder:
 BOOTCLASSPATH="-Xbootclasspath/a:$MARLIN_JAR_PREFIX.jar"
 
-# Optional Marlin 0.3 java2d patch in lib folder:
+# Optional Marlin java2d patch in lib folder:
 BOOTCLASSPATH="-Xbootclasspath/p:$MARLIN_JAR_PREFIX-sun-java2d.jar $BOOTCLASSPATH"
 
 # Marlin tuning options:
 USE_TL=true
-SIZE=8192
-#SIZE=2048
+SIZE=2048
+# larger pixelsize best for images ~ 8192x8192
+#SIZE=8192
 
 JAVA_OPTS="-Dsun.java2d.renderer.useThreadLocal=$USE_TL -Dsun.java2d.renderer.pixelsize=$SIZE -Dsun.java2d.renderer=org.marlin.pisces.PiscesRenderingEngine $JAVA_OPTS"
 
